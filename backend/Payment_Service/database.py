0751c28066b63cc6e66990fcf03d6d9788c0865a
Mark Regie A. Magtangob <marklegend029@gmail.com>
2025-10-22 22:15:56 +0800

    `Updated database server credentials across multiple services`

--- backend/Payment_Service/database.py
+++ backend/Payment_Service/database.py
@@ -1,12 +1,11 @@
 import aioodbc
 
 # database config
-server = 'DESKTOP-VQQ0NIU\SQLEXPRESS'
+server = 'DESKTOP-D84FNH8'
 database = 'OOS'
 username = 'sa'
-password = 'imsadmin'
-driver = 'ODBC Driver 17 for SQL Server'
-# async function to get db connection
+password = 'markregie123'
+driver = 'ODBC Driver 17 for SQL Server'# async function to get db connection
 async def get_db_connection():
     dsn = (
         f"DRIVER={{{driver}}};"
