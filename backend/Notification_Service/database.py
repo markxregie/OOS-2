c8c22b9e35c90df8ff6d42ca1427b69760ba825f
HydroX04 <dynartubigan.1@gmail.com>
2025-11-10 18:06:00 +0800

    1110

--- backend/Notification_Service/database.py
+++ backend/Notification_Service/database.py
@@ -2,11 +2,12 @@ import aioodbc
 
 
 # database config
-server = 'DESKTOP-D84FNH8'
+server = 'DESKTOP-FH6B6B4\SQLEXPRESS'
 database = 'OOS'
-username = 'sa'
-password = 'markregie123'
-driver = 'ODBC Driver 17 for SQL Server'# async function to get db connection
+username = 'imsadmin'
+password = 'imsadmin'
+driver = 'ODBC Driver 17 for SQL Server'
+# async function to get db connection
 async def get_db_connection():
     dsn = (
         f"DRIVER={{{driver}}};"
